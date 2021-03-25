baef0b327d7f471ccea42c42b45eb7b2e230cb18
Martin Peeters <martin.peeters@affinitic.be>
2021-03-25 18:30:03 +0100

    DMS-402: Fix tests and buildout

--- setup.py
+++ setup.py
@@ -66,8 +66,9 @@ setup(
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            "plone.testing>=5.0.0",
+            "plone.testing",
             "plone.app.robotframework[debug]",
+            "plone.app.contenttypes",
         ]
     },
     entry_points="""
