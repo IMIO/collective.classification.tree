2645264ce40a7c70da65191d6750b390f32d0fe5
Martin Peeters <martin.peeters@affinitic.be>
2021-04-19 08:38:09 +0200

    DMS-402: Add specific vocabularies for import

--- src/collective/classification/tree/vocabularies.py
+++ src/collective/classification/tree/vocabularies.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from collective.classification.tree import _
+from collective.classification.tree import utils
 from operator import itemgetter
+from plone import api
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
-from plone import api
-from collective.classification.tree import utils
+
+
+def iterable_to_vocabulary(values):
+    return SimpleVocabulary(
+        [SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in values]
+    )
 
 
 def classification_tree_vocabulary_factory(context):
@@ -18,7 +25,25 @@ def classification_tree_vocabulary_factory(context):
             ]
         )
     results = sorted(results, key=itemgetter(1))
-    terms = [
-        SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in results
-    ]
-    return SimpleVocabulary(terms)
+    return iterable_to_vocabulary(results)
+
+
+def csv_separator_vocabulary_factory(context):
+    values = (
+        (u";", u";"),
+        (u",", u","),
+        (u"|", u"|"),
+        (u"	", _(u"tab")),  # ! value is a tab not a whitespace !
+        (u" ", _(u"whitespace")),
+    )
+    return iterable_to_vocabulary(values)
+
+
+def import_keys_vocabulary_factory(context):
+    values = (
+        (u"parent_identifier", _(u"Parent Identifier")),
+        (u"identifier", _(u"Identifier")),
+        (u"title", _(u"Name")),
+        (u"informations", _(u"Informations")),
+    )
+    return iterable_to_vocabulary(values)
