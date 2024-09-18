534ce69f6be458d0428ae8322903793b22925d1c
Stephan Geulette <s.geulette@imio.be>
2024-09-18 14:08:49 +0200

    Preparing release 1.1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.classification.tree",
-    version="1.1.1.dev0",
+    version="1.1.1",
     description="Addon to manage complex content classification tree",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
